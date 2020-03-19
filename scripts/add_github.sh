3447ed5ae4b06571d5baccdb67d9ac643ac4219b
Charles Merriam <charles.merriam@gmail.com>
2020-03-18 18:09:53 -0700

    GitHub:  Forgot repository field

--- scripts/add_github.sh
+++ scripts/add_github.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 cat << EOF
 # Step 1:  Add GitHub repo
-  Got to your github page, 
+  Got to your github page,
   click '+' in the upper right, choose 'New Repository'
   Pick a name, add an optional description.  Don't add a README or .gitignore.  Hit Create Repository
   Copy the ssh url, it should match "git@github.com:<username>:<repo_name>"
@@ -21,9 +21,13 @@ There are ways to do this with the 'git add' command, but they do screw things u
 Use the "circle of arrows" icon on the bottom left of visual code, or use "git push origin" on the command line.
 Check your github page and verify you have content.
 
-# Step 4:  npm
+# Step 4:  package.json
 Copy this file into scripts
 chmod +x scripts/add_github.sh
 Add '"addgithub": "scripts/add_github.sh",' to package.json tasks
-
+Add a repository section like this:
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/merriam/bagels.git"
+  },
 EOF
